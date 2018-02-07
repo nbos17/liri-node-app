327e07ebe9302fac6b1dfae804d447ef59dd860f
Your Name <nbos0531@gmail.com>
2018-02-06 19:35:48 -0600

    changes1

--- liri.js
+++ liri.js
@@ -1,5 +1,10 @@
 require("dotenv").config();
 
-var spotify = new spotify(keys.spotify);
+var keys = require('./keys.js');
+var Twitter = require('Twitter');
+var Spotify = require('node-spotify-api');
+var Request = require("request");
+var fs = require('fs');
+var spotifyKey = new Spotify(keys.spotify);
 var client = new Twitter(keys.twitter);
 
