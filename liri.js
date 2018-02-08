e635eb000faf8ef27f0b58c1d59359553481342a
Your Name <nbos0531@gmail.com>
2018-02-07 18:15:42 -0600

    changes3

--- liri.js
+++ liri.js
@@ -8,3 +8,17 @@ var fs = require('fs');
 var spotifyKey = new Spotify(keys.spotify);
 var client = new Twitter(keys.twitter);
 
+// Read in the command line arguments
+var cmdArgs = process.argv;
+
+// The LIRI command will always be the second command line argument
+var liriCommand = process.argv[2];
+
+console.log(liriCommand);
+
+var params = {screen_name: 'nodejs'};
+client.get('statuses/user_timeline', params, function(error, tweets, response) {
+  if (!error) {
+    console.log(tweets);
+  }
+});
